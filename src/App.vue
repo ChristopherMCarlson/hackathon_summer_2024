c0a9fa3bb463ce4cb42ade52d0348ad1cb998b30
ChristopherMCarlson <c.m.carlson13@gmail.com>
2024-07-06 21:02:25 -0600

    test commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-toolbar-title>{{ activeComponent }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true,
+      activeComponent: 'Home'
+    }),
+  }
+</script>
